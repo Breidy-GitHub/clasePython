28af1c73cb59530a6c9684c2f20cd5578a365788
NOBODY <nobodyspecial_@outlook.com>
2023-03-25 08:44:44 -0500

    Resolve merge conflict in Reto 5-1.py

--- Animal.py
+++ Animal.py
@@ -3,23 +3,34 @@ class Animal:
     nombre="" #Atributo
     edad=0
     #Metodos
-    def registraranimal(self):
-        nombre=input("Ingrese el nombre del animal: ")
-        edad=int(input("Ingrese la edad del animal: "))
-    def mostrarAnimal(self,nombre,edad):
+    def __init__(self,nombre,edad):
+        self.nombre=nombre
+        self.edad=edad
+    def registrarAnimal(self):
+        self.nombre=input("Ingrese el nombre del animal: ")
+        self.edad=int(input("Ingrese la edad del animal: "))
+    '''def mostrarAnimal(self,nombre,edad):
         #print("El nombre del animal es: ", nombre,"y la edad es: ", edad)
-        print(f"El nombre del animal es: {nombre} y la edad es {edad}")
+        print(f"El nombre del animal es: {nombre} y la edad es {edad}")'''
+    def mostrarAnimal(self):
+        print(f"El nombre del animal es {self.nombre} y la edad es {self.edad}")
     #Definir metodo con valor de retorno
     def duplicarEdad(self,edad):
         duplicado=edad*2
         return duplicado
-#Crear objeto-> Instanciar Clase
+panda=Animal("Bambu",7)
+guacamayo=Animal(" ",0)
+
+guacamayo.registrarAnimal()
+guacamayo.mostrarAnimal()
+
+'''Crear objeto-> Instanciar Clase
 tigre=Animal()
 tigre.nombre="Tony"
 tigre.edad=5
 print("El nombre del animal es: ",tigre.nombre,"y su edad es: ", tigre.edad )
-#Invocar Metodo
+Invocar Metodo
 tigre.registraranimal()
 tigre.mostrarAnimal("Tony2",10)
 duplicar=tigre.duplicarEdad(5)
-print(f"La edad duplicada es: {duplicar}")
+print(f"La edad duplicada es: {duplicar}")'''
