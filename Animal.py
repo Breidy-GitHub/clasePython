9038a6d20caf082119a94dc41e3e28613c796b73
NOBODY <nobodyspecial_@outlook.com>
2023-03-25 08:54:44 -0500

    Resolve merge conflict in Reto 5-1.py

--- Animal.py
+++ Animal.py
@@ -1,28 +1,23 @@
 class Animal:
-    #Atributos
-    nombre=""
-    edad=0
-    #Metodos
-    def __init__(self,nombre,edad):
-        self.nombre=nombre
-        self.edad=edad
+    # Atributos
+    nombre = ""  # Atributo
+    edad = 0
+    # Métodos
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
     def registrarAnimal(self):
-        self.nombre=input("Ingrese el nombre del animal: ")
-        self.edad=int(input("Ingrese la edad del animal: "))
-    '''def mostrarAnimal(self,nombre,edad):
-        #print("El nombre del animal es: ", nombre,"y la edad es: ", edad)
-        print(f"El nombre del animal es: {nombre} y la edad es {edad}")'''
+        self.nombre = input("Ingrese el nombre del animal: ")
+        self.edad = int(input("Ingrese la edad del animal: "))
     def mostrarAnimal(self):
         print(f"El nombre del animal es {self.nombre} y la edad es {self.edad}")
-    #Definir metodo con valor de retorno
-    def duplicarEdad(self,edad):
-        duplicado=edad*2
-        return duplicado
-panda=Animal("Bambu",7)
-guacamayo=Animal(" ",0)
+
+panda = Animal("Bambu", 7)
+guacamayo = Animal(" ", 0)
 
 guacamayo.registrarAnimal()
 guacamayo.mostrarAnimal()
+
 
 '''Crear objeto-> Instanciar Clase
 tigre=Animal()
