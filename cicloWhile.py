b02eecfcab59505ee2891d312d4b79b951205271
NOBODY <nobodyspecial_@outlook.com>
2023-03-26 12:53:22 -0500

    Resolve merge conflict in Reto 5-1.py

--- cicloWhile.py
+++
@@ -1,6 +0,0 @@
-contin="s" #Declaracion variable con valor inicial que hace verdadera la condicion del ciclo
-
-while contin=="S" or contin=="S":
-    price=int(input("Ingrese el precio del producto \n"))
-    cantidad=int(input("Ingrese la cantidad del producto: "))
-    contin=input("Para continuar presione s/S o N/n para parar: ")
